9ea206ba68c38f976b7918a3f4ad7728912b1bb1
Jason <sjogrenjason33@gmail.com>
2026-01-23 15:09:10 -0600

    Bump version to 2.2.0 - Full cloud sync support

--- work_schedule_app/lib/services/update_service.dart
+++ work_schedule_app/lib/services/update_service.dart
@@ -12,7 +12,7 @@ class UpdateService {
   static const String _repo = 'Manager-Schedule-App';
 
   // Current app version (should match pubspec.yaml)
-  static const String currentVersion = '2.1.0';
+  static const String currentVersion = '2.2.0';
 
   /// Cached update info
   static String? _latestVersion;
