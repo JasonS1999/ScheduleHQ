a35d569810a1f186bb5d47af27f554e374e418b3
Jason <sjogrenjason33@gmail.com>
2026-02-09 01:32:51 -0600

    chore: remove dead code
    - Delete unused theme_service.dart (ValueNotifier, zero references)
    - Delete unused dashboard_page.dart (orphaned page, zero references)

--- ScheduleHQ_Desktop/lib/services/theme_service.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// App-level theme mode that can be listened to and changed at runtime.
-final ValueNotifier<ThemeMode> appThemeMode = ValueNotifier(ThemeMode.system);
